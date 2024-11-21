9ffecdc2604dbaabae23c058043bf7a9097d0ec9
AdamLunaAH <adam.a-h@outlook.com>
2024-11-21 10:35:30 +0100

    flashcard list interface

---
+++ FlipCards/Flashcard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCards
+{
+    internal class Flashcard : IFlaschcard
+    {
+
+
+        public int CardId { get; set; }
+        public string Title { get; set; }
+        public string Question { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Answer { get; set; }
+
+
+        public Flashcard(int cardId, string title, string question, string option1, string option2, string option3, string answer)
+        {
+            CardId = cardId;
+            Title = title;
+            Question = question;
+            Option1 = option1;
+            Option2 = option2;
+            Option3 = option3;
+            Answer = answer;
+        }
+
+    }
+}
