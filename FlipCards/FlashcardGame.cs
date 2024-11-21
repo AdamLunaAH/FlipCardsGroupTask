1ef1dc2c0ac247a248de5feb302ec81ec89ae5b5
Jonatan Präst <jonatan.prast@learnet.se>
2024-11-21 12:22:20 +0100

    .

--- FlipCards/FlashcardGame.cs
+++ FlipCards/FlashcardGame.cs
@@ -99,14 +99,58 @@ namespace FlipCards
             Console.WriteLine("All cards have been shuffled!");
         }
 
-        public void PlayRound()
+        public void PlayRound(List<IFlashcard> cards)
         {
+            if (cards.Count == 0)
+            {
+                Console.WriteLine("No flashcards to play. Add some cards first!");
+                return;
+            }
+
+            // Slumpa ett kort från listan
+            Random random = new Random();
+            Flashcard currentCard = (Flashcard)cards[random.Next(cards.Count)];
+
+            // Visa frågan och alternativen
+            Console.WriteLine($"Question: {currentCard.Question}");
+            Console.WriteLine($"1. {currentCard.Option1}");
+            Console.WriteLine($"2. {currentCard.Option2}");
+            Console.WriteLine($"3. {currentCard.Option3}");
+
+
+            Console.WriteLine("Choose your answer 1/2/3");
+            string answer = Console.ReadLine();
+
+            string correctAnswer = currentCard.Answer;
+
+            bool isCorrect = false;
+
+            // Kontrollera om svaret är korrekt
+            if ((answer == "1" && correctAnswer == currentCard.Option1) ||
+               (answer == "2" && correctAnswer == currentCard.Option2) ||
+               (answer == "3" && correctAnswer == currentCard.Option3))
+            {
+                isCorrect = true;
+                score++;
+            }
+
+            if (isCorrect)
+            {
+                Console.WriteLine("Right answer");
+            }
+
+            else
+            {
+                Console.WriteLine($"Wrong answer. Right answer was {currentCard.Answer}");
+            }
+
+            Console.WriteLine($"Current score: {score}");
 
         }
 
         public string GetScore()
         {
-            return $"";
+            return $"Current score: {score}";
         }
     }
 }
